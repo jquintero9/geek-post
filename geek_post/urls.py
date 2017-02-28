f8227e6fdb33cee1c8e129f491e5fff904a2b20c
Jhon Jaime Quintero <jquintero9@JHON-PC>
2017-02-28 11:11:44 -0500

    Agreando la ruta media

--- geek_post/urls.py
+++ geek_post/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
